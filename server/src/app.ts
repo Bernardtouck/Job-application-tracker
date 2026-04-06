744d2fe3401345d87b926055a2f6e9000fdaeda9
Bernardtouck <touckfils@gmail.com>
2026-04-06 19:50:24 +0200

    feat(app): mount /parse routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,14 +4,16 @@ import 'dotenv/config';
 
 import userRoutes from './routes/user.routes';
 import jobRoutes from "./routes/job.routes";
+import parserRoutes from "./routes/parser.routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/users', userRoutes);
-app.use('/auth', userRoutes);
-app.use('/jobs', jobRoutes);
+app.use('/users', userRoutes);   // POST /users (register), GET /users
+app.use('/auth', userRoutes);    // POST /auth/login
+app.use("/jobs",  jobRoutes);    // CRUD /jobs
+app.use('/parse', parserRoutes);     // POST /parse/text, POST /parse/image
 
 export default app;
