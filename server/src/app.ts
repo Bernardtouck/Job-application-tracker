1467aeb68c3cf2bb2a77189bf81fe81d09d1b4b5
Bernardtouck <touckfils@gmail.com>
2026-04-07 21:29:22 +0200

    feat(profile): add /users/profile routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,8 @@ import parserRoutes from "./routes/parser.routes";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "100mb" }));           
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
 app.use('/users', userRoutes);   // POST /users (register), GET /users
 app.use('/auth', userRoutes);    // POST /auth/login
