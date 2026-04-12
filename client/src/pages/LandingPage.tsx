c4a2985bbec30cbe7b4c0670dc9944c349290841
Bernardtouck <touckfils@gmail.com>
2026-04-12 22:47:17 +0200

    fix(landing): fix flip flash + full responsive with 4 breakpoints

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -148,7 +148,9 @@ function CardSide({ content, onFlip, onLogin, onRegister }: {
           <div className="lp-cta-group">
             <button className="lp-cta-primary" onClick={onRegister}>
               {content.cta}
-              <svg viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
+              <svg viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
+              </svg>
             </button>
             <button className="lp-cta-secondary" onClick={onLogin}>{content.login}</button>
           </div>
